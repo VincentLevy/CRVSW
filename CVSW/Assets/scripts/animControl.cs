6f5acb632b129c184617cf2550efbea0e2986763
VincentLevy <vicentelevy.a@gmail.com>
2020-04-07 00:56:30 -0600

    changed folders name

---
+++ CVSW/Assets/scripts/animControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//anim control on the prefab
+public class animControl : MonoBehaviour
+{
+    public Animator animator;
+
+    public void SetIsAttacking(bool value)
+    {
+        animator.SetBool("isAttacking", value);
+    }
+}
